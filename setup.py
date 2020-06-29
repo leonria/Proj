2b16e294d77f1c6c5dfda87697c29837b9a11734
LeonRia <b.rumyanzev@gmail.com>
2020-06-29 23:46:46 +0300

    add setup

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Hiddentechnologies',
+    version='',
+    packages=['HTech', 'HTech.migrations', 'hiddenTechnologies'],
+    url='',
+    license='',
+    author='oem',
+    author_email='',
+    description=''
+)
